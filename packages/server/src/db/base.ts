db6a34028dc95e202651475719705053919d0138
Zapic <kawaiizapic@zapic.moe>
2025-09-04 10:09:09 +0800

    fix: database initialization error

--- packages/server/src/db/base.ts
+++ packages/server/src/db/base.ts
@@ -2,28 +2,35 @@ import path from "tjs:path";
 import { Database } from "tjs:sqlite";
 
 export abstract class BaseDB {
-  protected db: Database;
+  static _db: Database;
   protected VERSION = 0;
 
   constructor(name: string) {
-    const db = new Database(path.join(APP_DIR, "data.db"), {
-      readOnly: false,
-      create: true
-    });
-    this.db = db;
+    const db = this.db;
+    db.exec("CREATE TABLE IF NOT EXISTS puraty_db_meta (name TEXT PRIMARY KEY, version TEXT)");
     try {
       const version = db.prepare("SELECT version FROM puraty_db_meta WHERE name=?").all(name)[0]?.version;
       if (typeof version !== "string") {
-        this.initialize();
+        throw new Error("Initialize required");
       } else if (version !== this.VERSION.toString()) {
         this.upgrade();
         db.prepare("UPDATE puraty_db_meta SET version=? WHERE name=?").run(this.VERSION, name);
       }
     } catch (e) {
-      db.exec("CREATE TABLE puraty_db_meta (name TEXT, version TEXT)");
       db.prepare("INSERT INTO puraty_db_meta (name, version) VALUES (?, ?)").run(name, this.VERSION);
       this.initialize();
     }
+  }
+
+  protected get db() {
+    if (!BaseDB._db) {
+      const db = new Database(path.join(APP_DIR, "data.db"), {
+        readOnly: false,
+        create: true
+      });
+      BaseDB._db = db;
+    }
+    return BaseDB._db;
   }
 
   protected abstract initialize(): void;
