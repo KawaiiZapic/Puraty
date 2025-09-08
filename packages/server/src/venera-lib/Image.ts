db196702a5b9497b0891d16699a6ec044c1058fd
Zapic <kawaiizapic@zapic.moe>
2025-09-08 09:31:16 +0800

    feat: operation based image processing

--- packages/server/src/venera-lib/Image.ts
+++ packages/server/src/venera-lib/Image.ts
@@ -1,38 +1,70 @@
 // jimp is too heavy for quickjs, takes up to 300 secs to process a png with 2M size
 // Considering using some native command line tool like imagemagick
 
+let CurrentImageKey = 0;
+
+const getImageKey = () => {
+    return ++CurrentImageKey;
+};
 export class Image {
     key = 0;
 
+    private _w: number = 0;
+    private _h: number = 0;
+    private _f?: string;
+    private _op: string[];
+
     constructor(key: number) {
         this.key = key;
+        this._op = [];
     }
 
-    copyRange(x: number, y: number, width: number, height: number): Image | null {
-        throw new Error("Calling not implemented method copyRange(x: number, y: number, width: number, height: number): Image | null");
+    copyRange(x: number, y: number, width: number, height: number): Image {
+        const r = new Image(getImageKey());
+        r._w = width;
+        r._h = height;
+        r._op = [`crop:${this.key}:${x}:${y}:${width}:${height}`];
+        return r;
     }
 
     copyAndRotate90() {
-        throw new Error("Calling not implemented method copyAndRotate90(): Image | null");
+        const r = new Image(getImageKey());
+        r._w = this._h;
+        r._h = this._w;
+        r._op = [`rotate:${this.key}:90`];
+        return r;
     }
 
     fillImageAt(x: number, y: number, image: Image) {
-        throw new Error("Calling not implemented method fillImageAt(x: number, y: number, image: Image): Image | null");
+        this._op.push(`fill:${image.key}:${x}:${y}`);
     }
 
     fillImageRangeAt(x: number, y: number, image: Image, srcX: number, srcY: number, width: number, height: number) {
-        throw new Error("Calling not implemented method fillImageRangeAt()");
+        this._op.push(`fill:${image.key}:${x}:${y}:${srcX}:${srcY}:${width}:${height}`);
     }
 
     get width() {
-        throw new Error("Calling not implemented getter width()")
+        return this._w;
     }
 
     get height() {
-        throw new Error("Calling not implemented getter height()")
+        return this._h;
+    }
+
+    get _source() {
+        return this._f;
     }
 
     static empty(width: number, height: number) {
-        throw new Error("Calling not implemented method empty()")
+        const r = new Image(getImageKey());
+        r._w = width;
+        r._h = height;
+        return r;
+    }
+
+    static fromFile(file: string) {
+        const r = new Image(getImageKey());
+        r._f = file;
+        return r;
     }
 }
