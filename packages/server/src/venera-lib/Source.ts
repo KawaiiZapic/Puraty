e6d4187c95194bcc46ff46728ea49afcfb81af63
Zapic <kawaiizapic@zapic.moe>
2025-09-09 15:50:32 +0800

    feat: base comic source interface

--- packages/server/src/venera-lib/Source.ts
+++ packages/server/src/venera-lib/Source.ts
@@ -1,11 +1,112 @@
 import { SourceData } from "@/db/SourceData";
 import { APP } from "./App";
+import type { Comic, ComicDetails, ImageLoadingConfig } from "./Data";
 
-interface SettingItem {
+interface AccountLogin {
+  login(account: string, pwd: string): Promise<unknown>;
+  loginWithWebview: {
+    url: string;
+    checkStatus(url: string, title: string): boolean;
+    onLoginSuccess(): void;
+  },
+  loginWithCookies: {
+    fields: string[];
+    validate: (values: string[]) => Promise<unknown>;
+  },
+  logout(): void;
+  registerWebsite: string;
+}
+
+interface PageJumpTarget {
+  page: "category" | "search";
+  attributes: Record<string, string>;
+}
+
+interface BaseExplorePage {
+  title: string;
+  load(page: number | null): Promise<unknown>;
+}
+
+interface MPPExplorePage extends BaseExplorePage {
+  type: "multiPartPage";
+  load(page: number | null): Promise<({title: string, comics: Comic[], viewMore: PageJumpTarget})[]>;
+}
+
+interface MPCLExplorePage extends BaseExplorePage {
+  type: "multiPageComicList";
+  load(page: number | null): Promise<{comics: Comic[], maxPage: number}>;
+  loadNext?: (next: unknown) => Promise<{comics: Comic[], next?: string}>;
+}
+
+interface MixedExplorePage extends BaseExplorePage {
+  type: "mixed";
+  load(page: number | null): Promise<{data: (Comic[] | {title: string, comics: Comic[], viewMore?: string})[], maxPage: number}>;
+}
+
+type AnyExplorePage = MPPExplorePage | MPCLExplorePage | MixedExplorePage;
+
+interface SearchOptionItem {
+  type?: "select" | "multi-select" | "dropdown";
+  options: string[];
+  label: string;
+  default?: string;
+}
+
+interface SearchOptions {
+  enableTagsSuggestions: boolean;
+  optionList: SearchOptionItem[];
+  load(keyword: string, options: string[], page: number): Promise<{comics: Comic[], maxPage: number}>;
+  loadNext(keyword: string, options: string[], next: string | null): Promise<{comics: Comic[], maxPage: number}>;
+}
+
+interface SelectSettingItem {
   type: "select";
   title: string;
-  options?: string[];
+  options?: ({ text?: string, value: string })[];
   default?: string;
+}
+
+interface SwitchSettingItem {
+  type: "switch";
+  title: string;
+  default?: boolean;
+}
+
+interface InputSettingItem {
+  type: "input";
+  title: string;
+  validator: null | string;
+  default?: string;
+}
+
+interface CallbackSettingItem {
+  type: "callback";
+  title: string;
+  buttonText: string;
+  callback(): void | Promise<void>;
+}
+
+type AnySettingItem = SelectSettingItem | SwitchSettingItem | InputSettingItem | CallbackSettingItem;
+
+interface ComicLoader {
+  loadInfo(id: string): Promise<ComicDetails>;
+  loadThumbnails?(id: string, next: string | null): Promise<{ thumbnails: string[], next?: string }>;
+  starRating?(id: string, rating: number): Promise<unknown>;
+  loadEp(comicId: string, epId?: string): Promise<{ images: string[] }>;
+  onImageLoad?(url: string, comicId: string, epId?: string): Promise<ImageLoadingConfig>;
+  onThumbnailLoad?(url: string): Promise<ImageLoadingConfig>;
+  likeComic?(id: string, isLike: boolean): Promise<unknown>;
+  loadComments?(comicId: string, subId: string | null, page: number, replyTo?: string): Promise<{ comments: Comment[], maxPage?: number }>;
+  sendComment?(comicId: string, subId: string | null, content: string, replyTo?: string): Promise<unknown>;
+  likeComment?(comicId: string, subId: string | null, commentId: string, isLike: boolean): Promise<unknown>;
+  voteComment?(comicId: string, subId: string | null, commentId: string, isUp: boolean, isCancel: boolean): Promise<unknown>;
+  idMatch?: string;
+  onClickTag?(namespace: string, tag: string): PageJumpTarget;
+  link?: {
+    domains: string[];
+    linkToId(url: string): string | null;
+  };
+  enableTagsTranslate?: boolean;
 }
 
 export abstract class ComicSource {
@@ -14,15 +115,27 @@ export abstract class ComicSource {
     public version = "";
     public minAppVersion = "";
     public url = "";
+    public settings?: Record<string, AnySettingItem>;
+    public account?: AccountLogin;
+    public explore?: AnyExplorePage[];
+    public search?: SearchOptions;
+    public comic: ComicLoader = {
+      loadInfo() {
+        throw new Error("Not implemented");
+      },
+      loadEp() {
+        throw new Error("Not implemented");
+      }
+    };
+
     private sd = SourceData.instance;
-    protected abstract settings: Record<string, SettingItem>;
 
     loadData(dataKey: string): string {
       return this.sd.get("data", this.key, dataKey);
     }
 
     loadSetting(key: string): string {
-      return this.sd.get("setting", this.key, key) ?? this.settings[key]?.default;
+      return this.sd.get("setting", this.key, key) ?? (this.settings?.[key] as InputSettingItem)?.default;
     }
 
     saveData(dataKey: string, data: string) {
