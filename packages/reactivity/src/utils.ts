9ea593230ea082c2ddb9462441f72994b8f45cfe
Zapic <kawaiizapic@zapic.moe>
2025-09-17 17:28:18 +0800

    fix: flush trigger wrong time

--- packages/reactivity/src/utils.ts
+++ packages/reactivity/src/utils.ts
@@ -1,8 +1,8 @@
 export const delayed = <T extends () => void>(fn: T): T => {
   let im: number | null = null;
   return ((...args) => {
+    im != null && clearTimeout(im);
     im = setTimeout(() => {
-      im != null && clearTimeout(im);
       fn.apply(null, args);
     });
   }) as T
