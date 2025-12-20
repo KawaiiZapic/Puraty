8e8fe6f1a55197d795669f209c7090bb65dfab63
Zapic <kawaiizapic@zapic.moe>
2025-12-20 13:39:46 +0800

    chore: remove usage of old render & reactive system

--- packages/reactivity/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-// @ts-check
-
-import { defineConfig } from "eslint/config";
-
-import ESLintConfig from "../tools/eslint.browser.config.mjs";
-
-export default defineConfig(...ESLintConfig, {
-	ignores: ["dist/**/*"]
-});
