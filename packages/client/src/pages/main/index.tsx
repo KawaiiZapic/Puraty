858411abaa543a156aec45514f3dbec25d9454b3
Zapic <kawaiizapic@zapic.moe>
2026-03-08 11:17:37 +0800

    feat(main): add a notice to add comic source if no source is installed

--- packages/client/src/pages/main/index.tsx
+++ packages/client/src/pages/main/index.tsx
@@ -1,11 +1,12 @@
 import type { InstalledSourceDetail } from "@puraty/server";
+import AddOutlined from "@sicons/material/AddOutlined.svg";
 import ExploreOutlined from "@sicons/material/ExploreOutlined.svg";
 
 import api from "@/api";
 import { List, ListIcon, ListItem, ListTitle } from "@/components";
 
 const MainPage = () => {
-	const [list, setList] = useState<InstalledSourceDetail[]>([]);
+	const [list, setList] = useState<InstalledSourceDetail[] | null>(null);
 	useEffect(() => {
 		api.ComicSource.list().then(setList);
 	}, []);
@@ -15,12 +16,24 @@ const MainPage = () => {
 				<ListTitle>
 					<ListIcon icon={ExploreOutlined}></ListIcon>探索
 				</ListTitle>
-				{list.map(item => (
-					<ListItem href={"/source/" + item.key} key={item.key}>
-						{item.name}
-					</ListItem>
-				))}
+				{list !== null &&
+					list.length > 0 &&
+					list.map(item => (
+						<ListItem href={"/source/" + item.key} key={item.key}>
+							{item.name}
+						</ListItem>
+					))}
 			</List>
+			{list !== null && list.length === 0 && (
+				<RouteLink
+					href="/settings/comic-sources"
+					class="flex flex-col items-center justify-center clickable-item opacity-60 py-6"
+				>
+					<img src={AddOutlined} class="w-16 h-16"></img>
+					<div class="my-2">未安装漫画源</div>
+					<div class="text-xs">点击添加</div>
+				</RouteLink>
+			)}
 		</div>
 	);
 };
